b2b484cb93a3cd7f2851b5d5e9f63948e85af6a4
Prince8311 <sourishmondal.vizac@gmail.com>
2025-06-21 00:01:06 +0530

    feat: admin auth

---
+++ _db-connect.php
@@ -0,0 +1,10 @@
+<?php
+
+    $server = "localhost";
+    $username = "ticketbay_abhay";
+    $password = 'abhay$ticketbay@2024';
+    $database = "ticketbay_ride";
+
+    $conn = mysqli_connect($server, $username, $password, $database);
+
+?>
