4c94a73dad783745d84d5f24854eac355407675d
Prince8311 <sourishmondal.vizac@gmail.com>
2025-06-25 03:43:36 +0530

    feat: employees

--- admin/auth/otp-verify.php
+++ admin/auth/otp-verify.php
@@ -1,7 +1,7 @@
 <?php 
 
 session_start();
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
 header('Content-Type: application/json');
