dce3961e319061766f39e73930b8e2ab15f4d7d8
Prince8311 <sourishmondal.vizac@gmail.com>
2025-07-15 02:10:02 +0530

    feat: role delete

---
+++ admin/api/roles/delete.php
@@ -0,0 +1,95 @@
+<?php 
+
+session_start();
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With');
+
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod == 'OPTIONS') {
+    header('HTTP/1.1 200 OK');
+    exit();
+}
+
+require "../../../utils/auth-helper.php";
+
+if ($requestMethod == 'POST') {
+
+    require "../../../_db-connect.php";
+    global $conn;
+
+    $authHeader = getAuthorizationHeader();
+    $cookieToken = $_COOKIE['authToken'] ?? '';
+
+    if (!isset($cookieToken) || empty($cookieToken)) {
+        $data = [
+            'status' => 401,
+            'message' => 'Authentication error'
+        ];
+        header("HTTP/1.0 401 Authentication error");
+        echo json_encode($data);
+    } else {
+        if (!$authHeader || !preg_match('/Bearer\s(\S+)/', $authHeader, $matches)) {
+            $data = [
+                'status' => 401,
+                'message' => 'Missing or malformed Authorization token',
+            ];
+            header("HTTP/1.0 401 Unauthorized");
+            echo json_encode($data);
+        } else {
+            $frontendToken = $matches[1];
+            if (empty($cookieToken) || $cookieToken !== $frontendToken) {
+                $data = [
+                    'status' => 401,
+                    'message' => 'Authentication mismatch',
+                ];
+                header("HTTP/1.0 401 Unauthorized");
+                echo json_encode($data);
+            } else {
+                $inputData = json_decode(file_get_contents("php://input"), true);
+                if (!empty($inputData)) {
+                    $roleName = mysqli_real_escape_string($conn, $inputData['roleName']);
+
+                    $deleteSQL = "DELETE FROM `roles_permissions` WHERE `role_name` = '$roleName'";
+                    $deleteResult = mysqli_query($conn, $deleteSQL);
+
+                    if($deleteResult) {
+                        $data = [
+                            'status' => 200,
+                            'message' => 'Role deleted successfully.'
+                        ];
+                        header("HTTP/1.0 200 Role deleted");
+                        echo json_encode($data);
+                    } else {
+                        $data = [
+                            'status' => 500,
+                            'message' => 'Database error: ' . $error
+                        ];
+                        header("HTTP/1.0 500 Internal Server Error");
+                        echo json_encode($data);
+                    }
+                } else {
+                    $data = [
+                        'status' => 400,
+                        'message' => 'Validation error'
+                    ];
+                    header("HTTP/1.0 400 Validation error");
+                    echo json_encode($data);
+                }
+            }
+        }
+    }
+
+} else {
+    $data = [
+        'status' => 405,
+        'message' => $requestMethod . ' Method Not Allowed',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
+
+?>
